5fb88310055894c6715b0886f3cf57a93ed649e1
Angy <angynewdev@gmail.com>
2026-05-29 00:56:36 +0000

    new change

--- frontend/inscription/src/utils/api.ts
+++ frontend/inscription/src/utils/api.ts
@@ -1,7 +1,7 @@
 import { FormData } from '../types'
 
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const API_URL = import.meta.env.VITE_API_URL
 const API_KEY = import.meta.env.VITE_API_KEY || ''
 
 
