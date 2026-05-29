25af81f0792aa372bc82e2f38767060fbc529714
Angy <angynewdev@gmail.com>
2026-05-29 01:59:09 +0000

    correction

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,7 +18,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['https://formulaire-backend0.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'https://formulaire-backend0.onrender.com', 
+    'localhost', 
+    '127.0.0.1',
+    ]
 
 
 # Application definition
