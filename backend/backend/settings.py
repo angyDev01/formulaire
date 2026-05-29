9ad89f2345313ce14dfbf5d556f13a50379af3b0
Angy <angynewdev@gmail.com>
2026-05-29 01:41:07 +0000

    correction of .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['https://formulaire-backend0.onrender.com', 'localhost', '127.0.0.1']
 
